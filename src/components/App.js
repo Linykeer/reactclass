3adc685a4c2745ac95797cfb47cacff692770c2a
Linykeer Almeida <linykeerrenato@gmail.com>
2021-01-11 20:32:12 -0300

    Projeto com classes react

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Post from './Post';
+
+export default class App extends React.Component {
+    render() {
+         return (
+           <div>
+             <h1>Hello World</h1>
+             <Post title="Aprendendo ReactJS com a RocketSeat"/>
+             <Post title="ReactJS é demais!!"/>
+             <Post title="Let's go "/>
+           </div>
+         )
+    }
+  
+}
